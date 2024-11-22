95b47644c5adcbab9bfc5ac31559fae6616421e2
Ryan <Adidea@users.noreply.github.com>
2024-11-22 12:26:46 -0500

    ignore mismatch keys sdxl_model_util.py
    Ignore mismatched keys in TE1 that prevents loading some SDXL models.

--- library/sdxl_model_util.py
+++ library/sdxl_model_util.py
@@ -257,7 +257,7 @@ def load_models_from_sdxl_checkpoint(model_version, ckpt_path, map_location, dty
         elif k.startswith("conditioner.embedders.1.model."):
             te2_sd[k] = state_dict.pop(k)
 
-    info1 = text_model1.load_state_dict(te1_sd)
+    info1 = text_model1.load_state_dict(te1_sd, strict=False)
     print("text encoder 1:", info1)
 
     converted_sd, logit_scale = convert_sdxl_text_encoder_2_checkpoint(te2_sd, max_length=77)
